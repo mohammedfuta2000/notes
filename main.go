ffc4c565dbe5d95cddcb60db9eb669f115576b24
mohammed <mohammedfuta@gmail.com>
2024-03-07 06:43:26 +0000

    interfaces- with wrapper functions - mimicing what happens in the internals

--- main.go
+++ main.go
@@ -2,36 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 )
 
-type book struct{
+type book struct {
 	title string
 }
 
-func (b book) String() string{
-	return fmt.Sprintf("The title of the Book is %v",b.title)
+func (b book) String() string {
+	return fmt.Sprintf("The title of the Book is %v", b.title)
 }
 
 type count int
 
-func (c count) String() string{
-	return fmt.Sprintf("This is the number %v",strconv.Itoa(int(c)) )
+func (c count) String() string {
+	return fmt.Sprintf("This is the number %v", strconv.Itoa(int(c)))
 }
 
 // now book and count are also of type stringer, so go itnernal will print them as depicted here
 
+func logInfo(s fmt.Stringer) {
+	log.Println("LOG FROM 138", s.String())
+}
 
+// type foo interface {
+// 	foofunc()
+// }
 
-func main()  {
-	b:= book{
+// type bar struct{}
+
+// func (b bar) foofunc() {}
+
+// func acceptor(f foo) {}
+
+func main() {
+	// var ba bar
+	// acceptor(ba)
+
+	b := book{
 		title: "gold and water",
 	}
 	var c count = 4
 
-	fmt.Printf("%v\n",b)
-	x:= fmt.Sprintln(c)
+	fmt.Printf("%v\n", b)
+	x := fmt.Sprintln(c)
 	fmt.Println(x)
+
+	// acceptor function
+	logInfo(b)
 	// notice how differntlyy it println prints them
 
 }
