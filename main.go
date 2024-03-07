f48af63088ebfa44f79cb9db1c209ad3a9df2bf1
mohammed <mohammedfuta@gmail.com>
2024-03-07 10:15:40 +0000

    closure

--- main.go
+++ main.go
@@ -2,34 +2,20 @@ package main
 
 import "fmt"
 
-// a func that retruns an int
-func foo() int  {
-	return 31
-}
-// a func that returns a differnet func
-func bar() (func()int){
-	return foo
-}
-
-
 func main() {
-	y:=bar()
-	fmt.Println(y())
-
-	// THIS IS COO;L;;
-	// fmt.Printf("%T",fmt.Println)
-	// fmt.Println(x)
-
-	result:=opp(1,1,add)
-	fmt.Println(result)
-}
-func opp(a,b int,f func(int,int)int) int {
-	return f(a,b)
+	f, i := incrementor()
+	fmt.Println(f())
+	f()
+	f()
+	f()
+	fmt.Println()
+	fmt.Println(*i)
 }
 
-func add(a,b int) int  {
-	return a+b
-}
-func sub(a,b int )int   {
-	return a-b
+func incrementor() (func() int, *int) {
+	x := 0
+	return func() int {
+		x++
+		return x
+	}, &x
 }
