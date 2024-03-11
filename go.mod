17b5ad826b0e4f14432875f93903788e240549e8
mohammed <mohammedfuta@gmail.com>
2024-03-11 11:46:17 +0000

    sort

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mohammedfuta2000/notes
 
 go 1.22.0
 
-require golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
+require (
+	golang.org/x/crypto v0.21.0
+	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
+)
--- main.go
+++ main.go
@@ -2,64 +2,73 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/exp/constraints"
+	"sort"
 )
 
-// type set
-type MyTS interface{
-	// int | float64
-	// ~int | ~float64
-	constraints.Integer | constraints.Float
+type user struct {
+	First   string
+	Last    string
+	Age     int
+	Sayings []string
 }
 
-// generics
-func add[T MyTS,U comparable](a, b T,c U) T {
-	return a+b
-}
+type ByAge []user
 
-// type alias and underlying type constraints
-type seconds int
+func (a ByAge) Len() int           { return len(a) }
+func (a ByAge) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByAge) Less(i, j int) bool { return a[i].Age < a[j].Age }
 
-type minutes = int
+type ByLast []user
 
-/*
-type MyStruct struct {
-	// Frequently accessed fields first
-	ID   int
-	Name string
-
-	// Group related fields together
-	Location struct {
-		Latitude  float64
-		Longitude float64
-	}
-
-	// Less frequently accessed or larger fields
-	Data []byte
-	Flag bool
-}
+func (l ByLast) Len() int           { return len(l) }
+func (l ByLast) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l ByLast) Less(i, j int) bool { return l[i].Last < l[j].Last }
 
 func main() {
-	m := MyStruct{
-		ID:   1,
-		Name: "futa",
-		Location: struct {
-			Latitude  float64
-			Longitude float64
-		}{
-			Latitude:  12.22,
-			Longitude: 12.22},
-		Data: []byte("gold"),
-		Flag: true}
-	m.Location.Latitude=12.33
-}
-*/
+	u1 := user{
+		First: "James",
+		Last:  "Bond",
+		Age:   32,
+		Sayings: []string{
+			"Shaken, not stirred",
+			"Youth is no guarantee of innovation",
+			"In his majesty's royal service",
+		},
+	}
+	u2 := user{
+		First: "Miss",
+		Last:  "Moneypenny",
+		Age:   27,
+		Sayings: []string{
+			"James, it is soo good to see you",
+			"Would you like me to take care of that for you, James?",
+			"I would really prefer to be a secret agent myself.",
+		},
+	}
+	u3 := user{
+		First: "M",
+		Last:  "Hmmmm",
+		Age:   54,
+		Sayings: []string{
+			"Oh, James. You didn't.",
+			"Dear God, what has James done now?",
+			"Can someone please tell me where James Bond is?",
+		},
+	}
 
-func main()  {
-	var n seconds = 20
+	users := []user{u1, u2, u3}
 
-	// man:=map[MyTS]string{}
+	sort.Sort(ByLast(users))
 
-	// var m minutes = 10
-	fmt.Println(add(n,2,"goof"))
+	for _, v := range users {
+		fmt.Printf("%v\t%v\t%v\n", v.First, v.Last, v.Age)
+		sort.Strings(v.Sayings)
+		for _, w := range v.Sayings {
+			fmt.Println("\t\t\t",w)
+		}
+
+	}
+
+	// your code goes here
+
 }
